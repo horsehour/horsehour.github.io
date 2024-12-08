6ed13cc080b672f56bfbd66f0d04b1b3a508e3e4
horsehour <jiangchunheng@gmail.com>
2024-12-07 23:26:40 -0800

    Updated cv.pdf and rewrote intro, added one new publication entry.

--- _site/update.sh
+++ _site/update.sh
@@ -5,6 +5,9 @@ bundle exec jekyll build
 # git add *
 # avoid file of size exceeding 100m
 
+# git config --global user.name horsehour
+# git config --global user.email jiangchunheng@gmail.com
+
 find * -size -100M -type f -print0 | xargs -0 git add
 git commit -a
 git push -u origin master
